b2e5349b7e5ef65f9dc3dc7b3dd949954c665bb9
Jan Caha <jan.caha@lutraconsulting.co.uk>
2023-11-08 14:51:05 +0100

    provider name

--- landxmlconvertor/text_constants.py
+++ landxmlconvertor/text_constants.py
@@ -2,5 +2,5 @@ class TextConstants:
     PLUGIN_NAME = "LandXML to Mesh"
     PLUGIN_ID = "landxmltomesh"
 
-    PLUGIN_PROVIDER_NAME = "LandXML2Mesh"
+    PLUGIN_PROVIDER_NAME = "LandXML To Mesh"
     PLUGIN_PROVIDER_ID = "landxml2mesh"
