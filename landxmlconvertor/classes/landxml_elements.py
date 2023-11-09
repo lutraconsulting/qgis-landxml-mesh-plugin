a37716393ab4877aeae09a9b96109e7db4bfac45
Jan Caha <jan.caha@lutraconsulting.co.uk>
2023-11-09 09:32:16 +0100

    default prefix 0 and convert to int

--- landxmlconvertor/classes/landxml_elements.py
+++ landxmlconvertor/classes/landxml_elements.py
@@ -8,13 +8,10 @@ from .mesh_elements import MeshFace, MeshVertex
 class LandXMLSurface:
     """Class for reading individual surface from LandXML"""
 
-    def __init__(self, surface: ET.Element, id_prefix: int = None) -> None:
+    def __init__(self, surface: ET.Element, id_prefix: int = 0) -> None:
         self.surface = surface
         self.name = self.surface.attrib["name"]
-        if id_prefix is None:
-            self.id_prefix = 0
-        else:
-            self.id_prefix = id_prefix
+        self.id_prefix = int(id_prefix)
 
         self.points: typing.List[MeshVertex] = []
         self._get_points()
