dbf38721f569a10a6c08db37022879b068fedcfd
Jan Caha <jan.caha@lutraconsulting.co.uk>
2023-11-15 10:46:02 +0100

    needs namespace and namespace prefix

--- landxmlconvertor/classes/landxml_elements.py
+++ landxmlconvertor/classes/landxml_elements.py
@@ -2,14 +2,27 @@ import copy
 import typing
 import xml.etree.ElementTree as ET
 
-from . import NS
 from .mesh_elements import MeshFace, MeshVertex
 
 
 class LandXMLSurface:
     """Class for reading individual surface from LandXML"""
 
-    def __init__(self, surface: ET.Element, surface_number: int, id_offset: int = 0) -> None:
+    def __init__(
+        self,
+        surface: ET.Element,
+        surface_number: int,
+        id_offset: int = 0,
+        namespace_prefix: str = "",
+        namespace: typing.Optional[typing.Dict[str, str]] = None,
+    ) -> None:
+        self.namespace_prefix = namespace_prefix
+
+        if namespace is not None:
+            self.namespace = namespace
+        else:
+            self.namespace = {"": ""}
+
         self.name = ""
 
         self.surface = surface
@@ -28,7 +41,7 @@ class LandXMLSurface:
 
     @property
     def _definition(self) -> ET.Element:
-        return self.surface.find("landxml:Definition", namespaces=NS)
+        return self.surface.find(f"{self.namespace_prefix}Definition", namespaces=self.namespace)
 
     def points(self, add_id_offset: bool = False) -> typing.List[MeshVertex]:
         """Returns deep copy of the vertices"""
@@ -54,24 +67,24 @@ class LandXMLSurface:
         if not self._definition:
             return
 
-        points = self._definition.find("landxml:Pnts", namespaces=NS)
+        points = self._definition.find(f"{self.namespace_prefix}Pnts", namespaces=self.namespace)
 
         if not points:
             return
 
-        for point_element in points.findall("landxml:P", namespaces=NS):
+        for point_element in points.findall(f"{self.namespace_prefix}P", namespaces=self.namespace):
             self._points.append(MeshVertex.from_xml_element(point_element))
 
     def _get_faces(self) -> None:
         if not self._definition:
             return
 
-        faces = self._definition.find("landxml:Faces", namespaces=NS)
+        faces = self._definition.find(f"{self.namespace_prefix}Faces", namespaces=self.namespace)
 
         if not faces:
             return
 
-        for i, face_element in enumerate(faces.findall("landxml:F", namespaces=NS)):
+        for i, face_element in enumerate(faces.findall(f"{self.namespace_prefix}F", namespaces=self.namespace)):
             attrs = face_element.attrib
             if "i" in attrs.keys():
                 try:
